3bc6712f98ecc21ac898f44bc63cb56ee689d8b9
jahed ahmed <jahed1961ahmed@gmail.com>
2024-05-01 22:28:50 +0600

    Initial commit

---
+++ indext.js
@@ -0,0 +1,11 @@
+document.getElementById("loginBtn").addEventListener("click", function () {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  // Here you can do whatever you want with the collected email and password
+  console.log("Email:", email);
+  console.log("Password:", password);
+  // For example, you can send them to a server for authentication
+  // Or perform client-side validation
+});
+
+// this code js have been taken from chatgpt
